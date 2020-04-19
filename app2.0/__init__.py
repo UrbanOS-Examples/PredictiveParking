6c4adfd820235822839732d1dd36f6dd914255af
Yuxiao Zhao <32104465+kldyzyx@users.noreply.github.com>
2020-04-19 18:43:01 -0400

    add cli now_adjusted predictor

---
+++ app2.0/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask, escape, request, jsonify
+from datetime import datetime
+from pytz import timezone
+from app import predictor
+from app import now_adjusted
+
+import logging
+logging.basicConfig(level=logging.NOTSET)
+
+app = Flask(__name__)
+
+@app.route('/api/v1/predictions')
+def predictions():
+    now = now_adjusted.adjust(datetime.now(timezone('US/Eastern')))
+    zoneParam = request.args.get('zone_ids')
+    if zoneParam != None:
+        zone_ids = zoneParam.split(',')
+    else:
+        zone_ids = 'All'
+
+    results = predictor.predict(now, zone_ids)
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run()
