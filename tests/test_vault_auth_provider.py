691ad0b0113fb2135f880231031a10395a9b9a98
Benjamin Brewer <ben@benbrewer.me>
2020-05-01 10:02:10 -0400

    minor udpates to move mocks out of main test

--- tests/test_vault_auth_provider.py
+++ tests/test_vault_auth_provider.py
@@ -4,14 +4,26 @@ import hvac
 import pytest
 from dotmap import DotMap
 
+
 def test_get_credentials_returns_dictionary_with_id_and_key(monkeypatch):
     access_key_id_from_vault = 'my_first_access_key_id'
     secret_access_key_from_vault = 'my_first_secret_key_value'
+
+    monkeypatch.setattr(hvac, 'Client', _mock_new_hvac_client_with_creds(access_key_id_from_vault, secret_access_key_from_vault))
+    monkeypatch.setattr('builtins.open', _mock_token_file_open)
     
-    def _mock_auth_kubernetes(_role, _jwt):
+    credentials = vault_auth_provider.get_credentials()
+
+    assert credentials['aws_access_key_id'] == access_key_id_from_vault
+    assert credentials['aws_secret_access_key'] == secret_access_key_from_vault
+
+
+def _mock_auth_kubernetes(_role, _jwt):
         return {}
 
-    def _mock_read_secret(_path, mount_point='mp'):
+
+def _mock_read_secret(access_key_id_from_vault, secret_access_key_from_vault):
+    def _closured(_path, mount_point='mp'):
         return {
             'data': {
                 'aws_access_key_id': access_key_id_from_vault,
@@ -19,30 +31,28 @@ def test_get_credentials_returns_dictionary_with_id_and_key(monkeypatch):
             }
         }
 
-    def _mock_new_client(vault_url):
+    return _closured
+
+def _mock_new_hvac_client_with_creds(access_key_id_from_vault, secret_access_key_from_vault):
+    def _closured(vault_url):
         return DotMap({
             'auth_kubernetes': _mock_auth_kubernetes,
             'secrets': {
                 'kv': {
                     'v1': {
-                        'read_secret': _mock_read_secret
+                        'read_secret': _mock_read_secret(access_key_id_from_vault, secret_access_key_from_vault)
                     }
                 }
             }
         })
 
-    def _mock_file_read():
-        return 'fake file contents'
+    return _closured
 
-    def _mock_file_open(file_path):
-        return DotMap({
-            'read': _mock_file_read
-        })
+def _mock_file_read():
+    return 'fake file contents'
 
-    monkeypatch.setattr(hvac, 'Client', _mock_new_client)
-    monkeypatch.setattr('builtins.open', _mock_file_open)
-    
-    credentials = vault_auth_provider.get_credentials()
 
-    assert credentials['aws_access_key_id'] == access_key_id_from_vault
-    assert credentials['aws_secret_access_key'] == secret_access_key_from_vault
+def _mock_token_file_open(file_path):
+    return DotMap({
+        'read': _mock_file_read
+    })
