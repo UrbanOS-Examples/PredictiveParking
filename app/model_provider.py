1fb1007a900e952f9a7dca27f88c43a3787dcdb0
Timothy Regan <tregan@pillartechnology.com>
2020-05-04 10:51:51 -0400

    configurable environment, cleaned comments

--- app/model_provider.py
+++ app/model_provider.py
@@ -1,4 +1,5 @@
 from os import path
+import os
 import numpy as np
 import pickle
 from cachetools import cached, TTLCache
@@ -24,25 +25,19 @@ def get_all(cluster_ids):
     session = boto3.Session(**credentials)
 
     s3 = session.resource('s3')
-    # TODO - make this configurable from the environment or at least convention based
-    bucket = s3.Bucket('dev-parking-prediction')
+    environment = os.getenv('SCOS_ENV') or 'dev'
+    bucket = s3.Bucket(environment + '-parking-prediction')
 
     models = {}
     base_model_file_path = mkdtemp()
 
     for cluster_id in cluster_ids:
         if not np.isnan(cluster_id):
-            # TODO - make this load each into a ByteIO so we don't leave files strewn all over the FS
-            # file_path = path.join(base_model_file_path, 'mlp_shortnorth_downtown_cluster' + str(int(cluster_id)))
-
             object_key = 'models/latest/mlp_shortnorth_downtown_cluster' + str(int(cluster_id))
-            # bucket.download_file(object_key, file_path)
-            print('downloading', object_key)
             with BytesIO() as bytesio:
                 bucket.download_fileobj(object_key, bytesio)
                 bytesio.seek(0)
                 loaded_model = pickle.load(bytesio)
-            # loaded_model = pickle.load(open(file_path, 'rb'))
 
             models[str(int(cluster_id))] = loaded_model
 
