5cf24ca914d4d6b84d330e89e0aa2b4f3c3792ec
Timothy Regan <tregan@pillartechnology.com>
2021-04-14 16:30:48 -0400

    Update ingress.yaml (#65)
    * Update ingress.yaml
    
    * Update deployment.yaml
    
    * update to terraform 0.12

--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
 provider "aws" {
   version = "2.70.0"
-  region  = "${var.region}"
+  region  = var.region
 
   assume_role {
-    role_arn = "${var.role_arn}"
+    role_arn = var.role_arn
   }
 }
 
 data "terraform_remote_state" "env_remote_state" {
   backend   = "s3"
-  workspace = "${terraform.workspace}"
+  workspace = terraform.workspace
 
-  config {
-    bucket   = "${var.alm_state_bucket_name}"
+  config = {
+    bucket   = var.alm_state_bucket_name
     key      = "operating-system"
     region   = "us-east-2"
-    role_arn = "${var.alm_role_arn}"
+    role_arn = var.alm_role_arn
   }
 }
 
 data "aws_secretsmanager_secret_version" "data_science_db_password" {
-  secret_id = "${data.terraform_remote_state.env_remote_state.data_science_db_secret_id}"
+  secret_id = data.terraform_remote_state.env_remote_state.outputs.data_science_db_secret_id
 }
 
 resource "local_file" "kubeconfig" {
   filename = "${path.module}/outputs/kubeconfig"
-  content = "${data.terraform_remote_state.env_remote_state.eks_cluster_kubeconfig}"
+  content  = data.terraform_remote_state.env_remote_state.outputs.eks_cluster_kubeconfig
 }
 
 resource "local_file" "helm_vars" {
@@ -36,21 +36,25 @@ ingress:
   annotations:
     alb.ingress.kubernetes.io/ssl-policy: "ELBSecurityPolicy-TLS-1-2-2017-01"
     alb.ingress.kubernetes.io/scheme: "${var.is_internal ? "internal" : "internet-facing"}"
-    alb.ingress.kubernetes.io/subnets: "${join(",", data.terraform_remote_state.env_remote_state.public_subnets)}"
-    alb.ingress.kubernetes.io/security-groups: "${data.terraform_remote_state.env_remote_state.allow_all_security_group}"
-    alb.ingress.kubernetes.io/certificate-arn: "${data.terraform_remote_state.env_remote_state.tls_certificate_arn},${data.terraform_remote_state.env_remote_state.root_tls_certificate_arn}"
+    alb.ingress.kubernetes.io/subnets: "${join(
+  ",",
+  data.terraform_remote_state.env_remote_state.outputs.public_subnets,
+)}"
+    alb.ingress.kubernetes.io/security-groups: "${data.terraform_remote_state.env_remote_state.outputs.allow_all_security_group}"
+    alb.ingress.kubernetes.io/certificate-arn: "${data.terraform_remote_state.env_remote_state.outputs.tls_certificate_arn},${data.terraform_remote_state.env_remote_state.outputs.root_tls_certificate_arn}"
     alb.ingress.kubernetes.io/tags: scos.delete.on.teardown=true
     alb.ingress.kubernetes.io/actions.redirect: '{"Type": "redirect", "RedirectConfig":{"Protocol": "HTTPS", "Port": "443", "StatusCode": "HTTP_301"}}'
     alb.ingress.kubernetes.io/listen-ports: '[{"HTTP": 80}, {"HTTPS": 443}]'
-    alb.ingress.kubernetes.io/wafv2-acl-arn: "${data.terraform_remote_state.env_remote_state.eks_cluster_waf_acl_arn}"
+    alb.ingress.kubernetes.io/wafv2-acl-arn: "${data.terraform_remote_state.env_remote_state.outputs.eks_cluster_waf_acl_arn}"
     alb.ingress.kubernetes.io/healthcheck-path: '/healthcheck'
-  rootDnsZone: "${data.terraform_remote_state.env_remote_state.root_dns_zone_name}"
+  rootDnsZone: "${data.terraform_remote_state.env_remote_state.outputs.root_dns_zone_name}"
   port: 80
 mssql:
-  serverUrl: "${data.terraform_remote_state.env_remote_state.data_science_db_server}"
-  userName: "${data.terraform_remote_state.env_remote_state.data_science_db_username}"
+  serverUrl: "${data.terraform_remote_state.env_remote_state.outputs.data_science_db_server}"
+  userName: "${data.terraform_remote_state.env_remote_state.outputs.data_science_db_username}"
   password: "${data.aws_secretsmanager_secret_version.data_science_db_password.secret_string}"
 EOF
+
 }
 
 resource "null_resource" "helm_deploy" {
@@ -66,12 +70,13 @@ helm upgrade --install predictive-parking ./chart --namespace=predictive-parking
     --values chart/${terraform.workspace}.yaml \
     ${var.extra_helm_args}
 EOF
+
   }
 
-  triggers {
+  triggers = {
     # Triggers a list of values that, when changed, will cause the resource to be recreated
     # ${uuid()} will always be different thus always executing above local-exec
-    hack_that_always_forces_null_resources_to_execute = "${uuid()}"
+    hack_that_always_forces_null_resources_to_execute = uuid()
   }
 }
 
